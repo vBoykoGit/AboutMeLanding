3a5480934b0964db91b2bbaabb3d20d765b3b124
Vladimir Boyko <vboyko024@gmail.com>
2019-01-07 16:32:56 +0700

    Header back + projects animation

--- about-me/src/components/Portfolio.js
+++ about-me/src/components/Portfolio.js
@@ -7,7 +7,12 @@ const Portfolio = () => {
         <section className='portfolio'>
             <div className='sectionTitle portfolioTitle'>Portfolio</div>
             <div className='slider'>
-                {[{ title: 'Stolby', type: 'iOS App' }, { title: 'Tatysev', type: 'iOS App' }, { title: 'Webinar', type: 'iOS App' }, { title: 'Anna.Money', type: 'iOS App' }, { title: 'Psorimetr', type: 'iOS App' }, { title: 'BeaApp', type: 'iOS App' }].map((i) =>
+                {[{ title: 'Stolby', type: 'iOS App', source: { title: 'AppStore', link: 'https://itunes.apple.com/ru/app/stolby/id1033572573?mt=8' } },
+                { title: 'Tatysev', type: 'iOS App', source: { title: 'AppStore', link: 'https://itunes.apple.com/ru/app/tatysev/id1071384712?l=en&mt=8' } },
+                { title: 'Webinar', type: 'iOS App', source: { title: 'AppStore', link: 'https://itunes.apple.com/ru/app/webinar/id1050809427?mt=8' } },
+                { title: 'Anna.Money', type: 'iOS App', source: { title: 'AppStore', link: 'https://itunes.apple.com/gb/app/anna-business/id1333008275ƒ' } },
+                { title: 'Psorimetr', type: 'iOS App', source: { title: 'AppStore', link: 'https://itunes.apple.com/ru/app/psorimetr/id1222487523?mt=8' } },
+                { title: 'BeaApp', type: 'iOS App', source: { title: 'AppStore', link: 'https://itunes.apple.com/ru/app/beapp/id1102359793?mt=8' } }].map((i) =>
                     (<Project key={i.title} title={i.title} type={i.type} />))}
             </div>
         </section>
--- about-me/src/components/Project.js
+++ about-me/src/components/Project.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import appStore from '../appStore.png';
 
-const Project = ({ title, type }) =>
+const Project = ({ title, type, source }) =>
     <div className='project'>
         <div className='projectInfo'>
             <img className='projectImage' src={appStore} alt="" />
             <div className='projectType'>{type}</div>
+            <div className='cover centeredColumn'>
+                <p>To AppStore</p>
+                <i className='arrow'></i>
+            </div>
         </div>
         <div className='projectTitle'>{title}</div>
     </div >
