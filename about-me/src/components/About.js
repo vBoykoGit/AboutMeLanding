12ced48f8f5025ba8b57579c43505b7f579e3045
Vladimir Boyko <vboyko024@gmail.com>
2019-01-12 00:17:13 +0700

    Content updates

--- about-me/src/components/About.js
+++ about-me/src/components/About.js
@@ -5,7 +5,7 @@ const About = (props) =>
     <section className='aboutSection'>
         <div className='about'>
             <div className='iam'>I Am a Creative Frontend Developer</div>
-            <div className='story'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. </div>
+            <div className='story'>Hello, my name is Vladimir Boyko. I have an experience with working on iOS applications. Now i’ve redeveloped my specialisation to the react-native and frontend development. My experience gives me and ability to work with different platforms screens and resolutions. My favourite technologies right now is React, ReactNative, Redux, TypeScript and Electrone. My priorities is to be part of healthy and kind community and opportunity to work with professional and experienced team. As developer i am working on Agile and Scrum, using tools like Kaiten, Trello and Jira. My experience with Continuous delivery and Continuous integration is related with using Circle CI and Fabric.</div>
             <img src={photo} className='photo' />
         </div>
     </section>
